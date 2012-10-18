da2a206d06e322b205b14e017f35fcd8032d6d3f
Michael Hoffer <info@michaelhoffer.de>
2012-10-18 19:59:17 +0200

    first tutorial added

---
+++ VFXWindows-Tutorial-1/src/vfxwindows/tutorial/pkg1/VFXWindowsTutorial1.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vfxwindows.tutorial.pkg1;
+
+import eu.mihosoft.vrl.fxwindows.Window;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Michael Hoffer <info@michaelhoffer.de>
+ */
+public class VFXWindowsTutorial1 extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        Pane canvas = new Pane();
+        Scene scene = new Scene(canvas, 600, 600);
+        
+        Window w = new Window("My Window");
+        w.setPrefSize(300, 200);
+        
+        canvas.getChildren().add(w);
+        
+        primaryStage.setTitle("VFXWindows Sample 01!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
