713b13ba8a8bf568c7742432ac92a4175c4fd793
Michael Hoffer <info@michaelhoffer.de>
2012-10-18 20:07:45 +0200

    comments added

--- VFXWindows-Tutorial-1/src/eu/mihosoft/vrl/fxwindows/tutorial/VFXWindowsTutorial1.java
+++ VFXWindows-Tutorial-1/src/eu/mihosoft/vrl/fxwindows/tutorial/VFXWindowsTutorial1.java
@@ -11,23 +11,36 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
- *
+ * First VFXWindows tutorial.
+ * 
  * @author Michael Hoffer <info@michaelhoffer.de>
  */
 public class VFXWindowsTutorial1 extends Application {
-    
+
     @Override
     public void start(Stage primaryStage) {
-        
+
+        // create the canvas where the windows will be added to
         Pane canvas = new Pane();
+        
+        // create a scene that displays the canvas (resolution 600,600)
         Scene scene = new Scene(canvas, 600, 600);
-        
+
+        // create a window with title "My Window"
         Window w = new Window("My Window");
+        
+        // set the window position to 10,10 (coordinates inside canvas)
+        w.setLayoutX(10);
+        w.setLayoutY(10);
+        
+        // define the initial window size
         w.setPrefSize(300, 200);
-        
+
+        // add the window to the canvas
         canvas.getChildren().add(w);
-        
-        primaryStage.setTitle("VFXWindows Sample 01!");
+
+        // init and show the stage
+        primaryStage.setTitle("VFXWindows Tutorial 01");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
